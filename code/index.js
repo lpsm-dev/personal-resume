9852c82068ba59d33fb3cb959c34c264305e8898
Lucca Pessoa <luccapsm@protonmail.com>
2021-12-19 15:14:07 -0300

    fix: output problems, import modules and organization code

--- code/index.js
+++ code/index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
-'use strict';
-
-var console = require('console');
-var inquirer = require('inquirer');
-var chalk = require('chalk');
+import chalk from 'chalk';
+import console from 'console';
+import inquirer from 'inquirer';
+import {readFile} from 'fs/promises';
 
 var response = chalk.bold.yellow;
-var resume = require('./assets/resume.json');
+
+const resume = JSON.parse(await readFile(new URL('./assets/resume.json', import.meta.url)));
 
 var resumePrompts = {
   type: 'list',
@@ -17,36 +17,49 @@ var resumePrompts = {
 };
 
 function resumeHandler() {
-  inquirer.prompt(resumePrompts).then((answer) => {
-    if (answer.resumeOptions == 'Exit') {
-      return;
-    }
-    var option = answer.resumeOptions;
-    console.log(response('--------------------------------------'));
-    resume[`${option}`].forEach((info) => {
-      console.log(response('|   => ' + info));
-    });
-    console.log(response('--------------------------------------'));
-    // console.log(resume[`${option}`]);
-    inquirer
-      .prompt({
-        type: 'list',
-        name: 'exitBack',
-        message: 'Go back or Exit?',
-        choices: ['Back', 'Exit'],
-      })
-      .then((choice) => {
-        if (choice.exitBack == 'Back') {
-          resumeHandler();
-        } else {
-          return;
-        }
+  inquirer
+    .prompt(resumePrompts)
+    .then(answer => {
+      if (answer.resumeOptions == 'Exit') {
+        return;
+      }
+      let option = answer.resumeOptions;
+      console.log(response('--------------------------------------'));
+      resume[`${option}`].forEach(info => {
+        console.log(response('|   => ' + info));
       });
-  });
+      console.log(response('--------------------------------------'));
+      inquirer
+        .prompt({
+          type: 'list',
+          name: 'exitBack',
+          message: 'Go back or Exit?',
+          choices: ['Back', 'Exit'],
+        })
+        .then(choice => {
+          if (choice.exitBack == 'Back') {
+            resumeHandler();
+          } else {
+            return;
+          }
+        })
+        .catch(function(err) {
+          console.log('Fetch problem');
+        });
+    })
+    .catch(function(err) {
+      console.log('Fetch problem');
+    });
+}
+
+function printWelcome() {
+  console.log(chalk.green('┌──────────────────────────────────────────────────────────────┐'));
+  console.log(chalk.green('│ Hello, My name is Lucca Pessoa and Welcome to my CLI Resume! │'));
+  console.log(chalk.green('└──────────────────────────────────────────────────────────────┘'));
 }
 
 function main() {
-  console.log('Hello, My name is Lucca Pessoa and Welcome to my CLI Resume');
+  printWelcome();
   resumeHandler();
 }
 
